2a831f3f96e48480fd37b490fb699e09c50c60a8
drworm75 <dwgames@gmail.com>
2017-02-18 11:15:18 -0600

    created augs to recreate solarsystem objects

--- javascripts/solarsystem.js
+++ javascripts/solarsystem.js
@@ -1,42 +1,21 @@
 var SolarSystem = (function(){
 
-	var planets = ["mercury", "venus", "earth", "mars", "jupiter", "saturn,", "uranus", "neptune"];
-	var dwarfPlanets = ["pluto"];
-	var numPlanetsPeopleLandedOn = 0;
 	
-	var stars = ["Sun", "Alpha Centauri", "Wolf 359"];
-	var solarSystemAge = 0;
+	var dwarfPlanets = ["pluto"];
+	
+	
+
+	
 
 	return {
-		getPlanets: function(){
-			return planets;
-		},
-		getPlanetsLandedPeopleOn: function(){
-			return numPlanetsPeopleLandedOn;
-		},
-		setPlanetsLandedPeopleOn: function(){
-			numPlanetsPeopleLandedOn++;
-		},
+
 		getDwarfPlanets: function(){
 			return "they are rejects";
-		},
-		getStars: function(){
-			return stars;
-		},
-		setStars: function(){
-			stars.push(newStar);
-		},
-		getSolarSystemAge: function(){
-			return solarSystemAge;	
-		},
-		setSolarSystemAge: function(){
-			solarSystemAge++;
-		}	
+		}
+
 		
 	}
 
 })();
 
-// console.log("Planets:", SolarSystem.getPlanets());
-// console.log("Planets with people:", SolarSystem.getPlanetsLandedPeopleOn());
-// console.log("Spaceships: ", SolarSystem.getSpaceships());
+
